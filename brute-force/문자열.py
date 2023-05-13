95bbbfd4e4c0f1a9dd7878febd29455f3c23fc94
minsuhaha <haminsu5@gmail.com>
2023-05-13 14:16:20 +0900

    brute-force-1120

--- brute-force/문자열.py
+++ brute-force/문자열.py
@@ -5,10 +5,11 @@ A, B = input().split()
 
 count = 0
 result = []
-for i in range(len(B)-len(A)+1): # 순서의 경우의 수도 고려해주기 위해서
+
+for i in range(len(B)-len(A)+1): # 순서의 경우의 수도 고려해주기 위해서 -> 끼워맞춰보는 활동
     count = 0
     for j in range(len(A)):
         if A[j] != B[i+j]:
             count+=1
     result.append(count)
-print(result)
+print(min(result))
