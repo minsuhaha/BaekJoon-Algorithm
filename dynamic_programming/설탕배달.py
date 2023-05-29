5d6fd5170626b42cc7837cfd67d3c8bbff8cac93
minsuhaha <haminsu5@gmail.com>
2023-05-29 20:51:20 +0900

    dp-설탕배달

--- dynamic_programming/설탕배달.py
+++ dynamic_programming/설탕배달.py
@@ -1,16 +1,21 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-# 봉지 3kg, 5kg 1
-bucket = 0
+# 설탕봉지는 3kg 짜리, 5kg 짜리가 있음
 
-while n >= 0:
-    # 0은 어떠한 수와 나눠도 0이라는 것을 기억!
-    if n % 5 == 0:
-        bucket += n//5
-        print(bucket)
+n = int(input()) # 18kg
+
+count = 0
+
+while n > 0:
+    if n % 5 == 0: # 가장 최상의 경우 -> 봉지가 가장 적게 사용됨
+        count += n // 5
         break
-    n = n - 3 # 3kg 봉지는 있기 때문에 하지만 최소한의 봉지개수를 위해서는 -3 해줌으로써 얼른 5의 배수를 만들어줘야함!
-    bucket += 1
-    if n < 0:
-        print(-1)
-    
+    else: 
+        if n >= 3:
+            n -= 3 # 우리의 목표는 5kg 봉지를 많이 사용하는것이기에 일단 3kg 봉지 하나만 사용하고 남은 설탕을 5kg 봉지들로만 가능한지 비교하기
+            count += 1 # 3kg 봉지 하나가 사용됨
+        else:
+            count = -1
+            break
+print(count)
