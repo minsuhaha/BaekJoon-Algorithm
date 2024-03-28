0632500fcec993cd9d58e104d5ead899dbaf1ed3
minsuhaha <haminsu5@gmail.com>
2024-03-28 14:05:18 +0900

    queue : 절대값 힙(11286)

---
+++ queue/절대값 힙(11286).py
@@ -0,0 +1,16 @@
+import sys
+from queue import PriorityQueue
+input = sys.stdin.readline
+n = int(input())
+
+queue = PriorityQueue()
+
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        if queue.empty():
+            print(0)
+        else:
+            print(queue.get()[1])
+    else:
+        queue.put((abs(num), num))
