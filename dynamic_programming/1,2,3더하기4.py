09885b839d102adc8b4576e59eea41d6cf36fb02
minsuhaha <haminsu5@gmail.com>
2024-05-26 15:28:47 +0900

    dynamic_programming : 1,2,3더하기 4 - G5

--- dynamic_programming/1,2,3더하기4.py
+++ dynamic_programming/1,2,3더하기4.py
@@ -1,27 +1,20 @@
-# n = int(input())
-# nums = [int(input()) for _ in range(n)]
+'''
+        1   2   3   4   5   6   7
+1       1   1   1   1   1   1   1
+1,2     1   2   2   3   3   3   4
+1,2,3   1   2   3   4   5   6   8
+'''
+import sys
+input = sys.stdin.readline
 
-# for num in nums:
-#     dp = [0] * (max(nums)+1)
-#     dp[0] = 1
-#     for i in range(1,4): # 1, 2, 3 의 숫자로 경우의 수를 만드는 것이니깐
-#         for j in range(i, num+1):
-#             dp[j] += dp[j-i]
-#     print(dp[num])
-
-
-num = int(input())
-lst = [int(input()) for i in range(num)]
-d = [1]*10001
-d[1] = 1
-d[2] = 2
-d[3] = 3
-
-for i in range(4,10001):
-    d[i] += d[i-2]
-
-for j in range(5, 10001):
-    d[i] += d[i-3]
-
-for i in lst:
-    print(d[i])
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    
+    dp = [1]*(n+1)
+    
+    for i in range(2, 4):
+        for j in range(i, n+1):
+            dp[j] += dp[j-i]
+    
+    print(dp[n])
