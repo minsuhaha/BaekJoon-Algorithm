9efb10835212ad22c53fedad708a35f4626cf9cd
minsuhaha <haminsu5@gmail.com>
2024-05-26 16:33:31 +0900

    implementation : 블로그 (21921) - S3

--- implementation/블로그 (21921번).py
+++ implementation/블로그 (21921번).py
@@ -1,22 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 n, x = map(int, input().split())
+visitor = list(map(int, input().split()))
 
-visit = list(map(int, input().split()))
+visit_cnt = sum(visitor[:x])
+max_visit = visit_cnt
+cnt = 1
 
-result = []
+# max_visit 값 찾기
+for i in range(1, n-x+1):
+    visit_cnt = visit_cnt - visitor[i-1] + visitor[x-1+i] # 윈도우 슬라이싱 -> sum을 계속해서 하는것보다 훨씬 효율적
+    if visit_cnt > max_visit:
+        max_visit = visit_cnt
+        cnt = 1 # max visit 값이 변경됄때마다 cnt=1로 초기화
+    elif visit_cnt == max_visit:
+        cnt += 1
 
-for i in range(n):
-    total = visit[i]
-
-    for j in range(i+1, i+x):
-        if j > n-1:
-            break
-        total += visit[j]
-
-    result.append(total)
-
-max_visit = max(result)
 if max_visit == 0:
     print('SAD')
 else:
-    print(max(result))
-    print(result.count(max(result)))
+    print(max_visit)
+    print(cnt)
